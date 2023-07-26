85d107df7520cc4e3b5b350024e3e9a5c789928e
Ajaymaurya1008 <ajaykumar.maurya215061@tsecol.onmicrosoft.com>
2023-07-26 18:47:38 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (38.98%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.java
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -1,20 +1,37 @@
-import java.util.*;
-import java.util.ArrayList;
-import java.util.Collections;
-
 class Solution {
-    public int maximumWealth(int[][] accounts) {
-        ArrayList<Integer> store = new ArrayList<Integer>();
-        for(int i=0;i<accounts.length;i++){
-            int addd=0;
-            for(int j=0;j<accounts[i].length;j++){
-                addd+=accounts[i][j];
-                
-            }
-            store.add(addd);
-        }
-        int f=Collections.max(store);
-        return f;
-    }
+   public int maximumWealth(int[][] accounts) {
+       int max=1;
+       for(int i=0;i<accounts.length;i++){
+           int rowSum = 0;
+           for(int j=0;j<accounts[i].length;j++){
+               rowSum+=accounts[i][j];
+           }
+           if(rowSum>max){
+               max=rowSum;
+           }
+       }
+       return max;
+   }
 }
+
+
+// import java.util.*;
+// import java.util.ArrayList;
+// import java.util.Collections;
+
+// class Solution {
+//     public int maximumWealth(int[][] accounts) {
+//         ArrayList<Integer> store = new ArrayList<Integer>();
+//         for(int i=0;i<accounts.length;i++){
+//             int addd=0;
+//             for(int j=0;j<accounts[i].length;j++){
+//                 addd+=accounts[i][j];
+                
+//             }
+//             store.add(addd);
+//         }
+//         int f=Collections.max(store);
+//         return f;
+//     }
+// }
 

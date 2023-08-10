43f48a1043b8187157e6093603a74568ef1ae663
Ajaymaurya1008 <ajaykumar.maurya215061@tsecol.onmicrosoft.com>
2023-08-11 00:47:03 +0530

    Time: 2 ms (51.84%) | Memory: 41.3 MB (97.07%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -9,3 +9,32 @@ class Solution {
         return false;
     }
 }
+
+
+
+
+
+
+
+        // Arrays.sort(arr);
+        // int m =-1;
+        // for(int i=0;i<arr.length;i++){
+        //     m = Arrays.binarySearch(arr,arr[i]*arr[i]);
+        //     if(m>0)
+        //         return true;
+        // }
+        // return false;
+
+           // start = i + 1;
+            // end = arr.length - i;
+            // while(start<=end){
+            //     mid = start + (end-start)/2;
+            //     if(arr[mid]>arr[i]*2)
+            //         end = mid -1;
+            //     else if(arr[mid]<arr[i]*2)
+            //         start = mid + 1;
+            //     else{
+            //         return true;
+            //     }
+            // }
+    

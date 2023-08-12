be66458a86a2c0e3894136167913612e8f77a827
Ajaymaurya1008 <ajaykumar.maurya215061@tsecol.onmicrosoft.com>
2023-08-12 11:14:54 +0530

    Time: 1 ms (24.14%) | Memory: 41.5 MB (85.44%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.java
@@ -1,12 +1,31 @@
+// class Solution {
+//     public int findKthPositive(int[] arr, int k) {
+//         ArrayList<Integer> list = new ArrayList<Integer>();
+//         for(int i=1;i<=arr[arr.length-1]+k+1;i++){
+//             list.add(i);
+//         }
+//         for(int i=0;i<arr.length;i++){
+//             list.remove(Integer.valueOf(arr[i]));
+//         }
+//         return list.get(k-1);
+//     }
+// }
+
 class Solution {
     public int findKthPositive(int[] arr, int k) {
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        for(int i=1;i<=arr[arr.length-1]+k+1;i++){
-            list.add(i);
+        int num = 1; // Current positive integer to check
+        int count = 0; // Count of missing positive integers
+        int i = 0; // Index for the input array
+        
+        while (count < k) {
+            if (i < arr.length && arr[i] == num) {
+                i++; // Move to the next element in the array
+            } else {
+                count++; // Increment the count of missing positive integers
+            }
+            num++; // Move to the next positive integer
         }
-        for(int i=0;i<arr.length;i++){
-            list.remove(Integer.valueOf(arr[i]));
-        }
-        return list.get(k-1);
+        
+        return num - 1; // The k-th missing positive integer
     }
 }

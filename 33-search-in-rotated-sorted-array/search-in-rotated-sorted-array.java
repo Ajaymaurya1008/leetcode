7a3e1b459908c56b65056e05dc920b159d7805bb
Ajaymaurya1008 <ajaykumar.maurya215061@tsecol.onmicrosoft.com>
2023-08-24 01:20:10 +0530

    Time: 5 ms (5.01%) | Memory: 40.8 MB (70.80%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -34,25 +34,22 @@ class Solution {
         
     
     
-    static int findPivot(int[] arr) {
-        int start = 0;
-        int end = arr.length - 1;
+    public int findPivot(int[] arr){
+        int start  =0;
+        int end = arr.length-1;
         int mid=0;
-        while (start <= end) {
-            mid = start + (end - start) / 2;
-            // 4 cases over here
-            if (mid < end && arr[mid] > arr[mid + 1]) {
+        while(start<=end){
+            mid = start + (end-start)/2;
+            if(mid<end && arr[mid]>arr[mid+1])
                 return mid;
-            }
-            if (mid > start && arr[mid] < arr[mid - 1]) {
+            else if(mid>start && arr[mid]<arr[mid-1])
                 return mid-1;
-            }
-            if (arr[mid] <= arr[start]) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
+            else if(arr[mid]<=arr[start])
+                end=mid-1;
+            else
+                start=mid+1;
         }
         return mid;
     }
+    
 }

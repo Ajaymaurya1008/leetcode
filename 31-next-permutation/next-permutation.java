1c8eb5ed2ef6c86763beee55ede3f9a800e79b01
Ajaymaurya1008 <ajaykumar.maurya215061@tsecol.onmicrosoft.com>
2023-08-24 23:45:48 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (93.18%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,36 +1,22 @@
 class Solution {
-    public void nextPermutation(int[] n) {
-        //breakpoint
-       if(  n==null || n.length<=1)return;
-		
-		int i = n.length-2;
-
-		while(i>=0 && n[i]>=n[i+1])i--;
-		
-		int j=n.length-1;
-		
-		if(i>=0) {
-			while(n[j]<=n[i])j--;
-			swap(n,i,j);
-		}
-		
-		reverse(n,i+1,n.length-1);
-		
-		for(int p=0;p<n.length;p++) {
-			System.out.println(n[p]);
-		}
-		
-	}
-	public static void swap(int n[],int i,int j) {
-		int temp=n[i];
-		n[i]=n[j];
-		n[j]=temp;
-	}
-	public static void reverse(int n[],int i,int j) {
-		while(i<j) {
-			swap(n,i,j);
-			i++;
-			j--;
-		}
-	}
+    public void nextPermutation(int[] A) {
+        
+    if(A == null || A.length < 2) return;
+    int i = A.length - 2;
+    while(i >= 0 && A[i] >= A[i + 1]) i --;
+    if(i >= 0) {
+        int j = A.length - 1;
+        while(A[j] <= A[i]) j --;
+        swap(A, i, j);
+    }
+    reverse(A, i + 1, A.length - 1);
+ }
+     public void swap(int[] A, int i, int j) {
+      int tmp = A[i];
+      A[i] = A[j];
+      A[j] = tmp;
+    }
+     public void reverse(int[] A, int i, int j) {
+    while(i < j) swap(A, i ++, j --);
+    }
 }

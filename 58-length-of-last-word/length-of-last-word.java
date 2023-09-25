7f9489b0504d0c69478f43e7a3243ef0da9fee2b
Ajaymaurya1008 <ajaykumar.maurya215061@tsecol.onmicrosoft.com>
2023-09-25 21:36:00 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (72.91%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,6 +1,17 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] b = s.split(" ");
-        return b[b.length-1].length();
+        String m = s.trim();
+        int count =0;
+        for(int i=m.length()-1;i>=0;i--){
+            if(m.charAt(i) != ' '){
+                count++;
+            }
+            if(m.charAt(i)==' '){
+                break;
+            }
+        }
+        return count;
+        // String[] b = s.split(" ");
+        // return b[b.length-1].length();
     }
 }

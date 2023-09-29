5c164609eb0023d9c79e3a677b8744579926f4c0
Ajaymaurya1008 <ajaykumar.maurya215061@tsecol.onmicrosoft.com>
2023-09-29 21:23:55 +0530

    Time: 1 ms (100.00%) | Memory: 40.8 MB (95.93%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -1,10 +1,33 @@
 import java.math.BigInteger;
 class Solution {
     public String addBinary(String a, String b) {
-        BigInteger p = new BigInteger(a,2);
-        BigInteger q = new BigInteger(b,2);
-        BigInteger sum = p.add(q);
-        return sum.toString(2);
+        StringBuilder ans = new StringBuilder();
+        int n1 = a.length()-1;
+        int n2 = b.length()-1;
+        int carry = 0,base=2;
+        while(n1>=0 || n2>=0){
+            int num1 =0,num2 =0,sum;
+            if(n1>=0)
+                num1 = a.charAt(n1--) - '0';
+            if(n2>=0)
+                num2 = b.charAt(n2--) - '0';
+            sum = num1 + num2 + carry; // sum = 1 + 0 + 1 = 0 & 1 carry
+            if(sum>=base){
+                carry = 1;
+                sum = sum - base;
+            }else{
+                carry = 0;
+            }
+            ans.append(sum);
+        }
+        if(carry==1){
+            ans.append(1);
+        }
+        return ans.reverse().toString();
+        // BigInteger p = new BigInteger(a,2);
+        // BigInteger q = new BigInteger(b,2);
+        // BigInteger sum = p.add(q);
+        // return sum.toString(2);
         // return Integer.toBinaryString(Integer.parseInt(a,2)+Integer.parseInt(b,2));
     }
 }

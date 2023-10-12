89236d1f2528f30657b5417acc263aa969152801
Ajaymaurya1008 <ajaykumar.maurya215061@tsecol.onmicrosoft.com>
2023-10-12 17:41:41 +0530

    Time: 2 ms (97.35%) | Memory: 47.1 MB (13.37%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -18,23 +18,57 @@
 //         return list;
 //     }
 // }
+// public class Solution {
+//     public List<Integer> majorityElement(int[] nums) {
+//         List<Integer> result = new ArrayList<>();
+//         Map<Integer, Integer> counts = new HashMap<>();
+//         int n = nums.length;
+//         int threshold = n / 3;
+
+//         for (int num : nums) {
+//             counts.put(num, counts.getOrDefault(num, 0) + 1);
+//         }
+
+//         for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+//             if (entry.getValue() > threshold) {
+//                 result.add(entry.getKey());
+//             }
+//         }
+
+//         return result;
+//     }
+// }
+
+
 public class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> result = new ArrayList<>();
-        Map<Integer, Integer> counts = new HashMap<>();
-        int n = nums.length;
-        int threshold = n / 3;
-
+        int candidate1 = 0, candidate2 = 0, count1 = 0, count2 = 0;
+        
         for (int num : nums) {
-            counts.put(num, counts.getOrDefault(num, 0) + 1);
-        }
-
-        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
-            if (entry.getValue() > threshold) {
-                result.add(entry.getKey());
+            if (candidate1 == num) count1++;
+            else if (candidate2 == num) count2++;
+            else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
             }
         }
-
-        return result;
+        
+        count1 = count2 = 0;
+        for (int num : nums) {
+            if (num == candidate1) count1++;
+            else if (num == candidate2) count2++;
+        }
+        
+        List<Integer> ans = new ArrayList<>();
+        if (count1 > nums.length / 3) ans.add(candidate1);
+        if (count2 > nums.length / 3) ans.add(candidate2);
+        
+        return ans;
     }
 }

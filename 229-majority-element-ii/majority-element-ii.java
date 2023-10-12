893b52dc573a1e4d342924cb922ba2ce370dcfbe
Ajaymaurya1008 <ajaykumar.maurya215061@tsecol.onmicrosoft.com>
2023-10-12 17:40:48 +0530

    Time: 11 ms (39.28%) | Memory: 46.7 MB (34.24%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,20 +1,40 @@
-class Solution {
+// class Solution {
+//     public List<Integer> majorityElement(int[] nums) {
+//         ArrayList<Integer> list = new ArrayList<>();
+//         HashMap<Integer,Integer> map = new HashMap<>();
+//         for(int i:nums){
+//             if(!map.containsKey(i)){
+//                 map.put(i,1);
+//             }else{
+//                 map.put(i,map.get(i)+1);
+//             }
+//         }
+//         int n = nums.length;
+//         for(Map.Entry<Integer,Integer> ent : map.entrySet()){
+//             if(ent.getValue()>n/3){
+//                 list.add(ent.getKey());
+//             }
+//         }
+//         return list;
+//     }
+// }
+public class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        ArrayList<Integer> list = new ArrayList<>();
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i:nums){
-            if(!map.containsKey(i)){
-                map.put(i,1);
-            }else{
-                map.put(i,map.get(i)+1);
-            }
-        }
+        List<Integer> result = new ArrayList<>();
+        Map<Integer, Integer> counts = new HashMap<>();
         int n = nums.length;
-        for(Map.Entry<Integer,Integer> ent : map.entrySet()){
-            if(ent.getValue()>n/3){
-                list.add(ent.getKey());
+        int threshold = n / 3;
+
+        for (int num : nums) {
+            counts.put(num, counts.getOrDefault(num, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+            if (entry.getValue() > threshold) {
+                result.add(entry.getKey());
             }
         }
-        return list;
+
+        return result;
     }
 }

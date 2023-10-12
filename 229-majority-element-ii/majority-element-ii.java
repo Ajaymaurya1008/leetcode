17830b510fd2a208004b4c34f97f61a56ec9aee7
Ajaymaurya1008 <ajaykumar.maurya215061@tsecol.onmicrosoft.com>
2023-10-12 17:44:52 +0530

    Time: 12 ms (28.42%) | Memory: 46.2 MB (75.91%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,74 +1,20 @@
-// class Solution {
-//     public List<Integer> majorityElement(int[] nums) {
-//         ArrayList<Integer> list = new ArrayList<>();
-//         HashMap<Integer,Integer> map = new HashMap<>();
-//         for(int i:nums){
-//             if(!map.containsKey(i)){
-//                 map.put(i,1);
-//             }else{
-//                 map.put(i,map.get(i)+1);
-//             }
-//         }
-//         int n = nums.length;
-//         for(Map.Entry<Integer,Integer> ent : map.entrySet()){
-//             if(ent.getValue()>n/3){
-//                 list.add(ent.getKey());
-//             }
-//         }
-//         return list;
-//     }
-// }
-// public class Solution {
-//     public List<Integer> majorityElement(int[] nums) {
-//         List<Integer> result = new ArrayList<>();
-//         Map<Integer, Integer> counts = new HashMap<>();
-//         int n = nums.length;
-//         int threshold = n / 3;
-
-//         for (int num : nums) {
-//             counts.put(num, counts.getOrDefault(num, 0) + 1);
-//         }
-
-//         for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
-//             if (entry.getValue() > threshold) {
-//                 result.add(entry.getKey());
-//             }
-//         }
-
-//         return result;
-//     }
-// }
-
-
-public class Solution {
+class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int candidate1 = 0, candidate2 = 0, count1 = 0, count2 = 0;
-        
-        for (int num : nums) {
-            if (candidate1 == num) count1++;
-            else if (candidate2 == num) count2++;
-            else if (count1 == 0) {
-                candidate1 = num;
-                count1 = 1;
-            } else if (count2 == 0) {
-                candidate2 = num;
-                count2 = 1;
-            } else {
-                count1--;
-                count2--;
+        ArrayList<Integer> list = new ArrayList<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i:nums){
+            if(!map.containsKey(i)){
+                map.put(i,1);
+            }else{
+                map.put(i,map.get(i)+1);
             }
         }
-        
-        count1 = count2 = 0;
-        for (int num : nums) {
-            if (num == candidate1) count1++;
-            else if (num == candidate2) count2++;
+        int n = nums.length;
+        for(Map.Entry<Integer,Integer> ent : map.entrySet()){
+            if(ent.getValue()>n/3){
+                list.add(ent.getKey());
+            }
         }
-        
-        List<Integer> ans = new ArrayList<>();
-        if (count1 > nums.length / 3) ans.add(candidate1);
-        if (count2 > nums.length / 3) ans.add(candidate2);
-        
-        return ans;
+        return list;
     }
 }

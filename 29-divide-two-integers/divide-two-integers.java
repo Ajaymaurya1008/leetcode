6e9342441a1f3b67d229704ea16a24f555ae7684
Ajay Maurya <ajaykumar.maurya215061@tsecol.onmicrosoft.com>
2023-10-23 21:58:05 +0530

    Time: 1 ms (84.96%) | Memory: 39.6 MB (43.44%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.java
+++ 29-divide-two-integers/divide-two-integers.java
@@ -1,8 +1,63 @@
 class Solution {
     public int divide(int dividend, int divisor) {
-        if(dividend==-2147483648 && divisor==-1){
-            return 2147483647;
+        if (divisor == 0) {
+            return 0;
         }
-        return dividend/divisor;
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+        int quotient = 0;
+        boolean negative = (dividend < 0) != (divisor < 0);
+        long longDividend = Math.abs((long) dividend);
+        long longDivisor = Math.abs((long) divisor);
+        while (longDividend >= longDivisor) {
+            int shift = 0;
+            while (longDividend >= (longDivisor << shift)) {
+                shift++;
+            }
+            shift--;
+            longDividend -= longDivisor << shift;
+            quotient += 1 << shift;
+        }
+        return negative ? -quotient : quotient;
     }
 }
+
+// class Solution {
+//     public int divide(int dividend, int divisor) {
+//         int count = 0;
+//         int a = Math.abs(dividend);
+//         if(dividend == Integer.MIN_VALUE){
+//             a = -Integer.MIN_VALUE-1;
+//         }
+//         if (dividend == Integer.MIN_VALUE && divisor == -1) {
+//             return Integer.MAX_VALUE;
+//         }
+//         if (dividend == Integer.MIN_VALUE && divisor == 2) {
+//             return -Integer.MAX_VALUE/2-1;
+//         }
+//         if(dividend == Integer.MIN_VALUE && divisor == 1) {
+//             return Integer.MAX_VALUE+1;
+//         }
+//         int b = Math.abs(divisor);
+//         if(divisor == Integer.MIN_VALUE){
+//             b = -Integer.MIN_VALUE-1;
+//         }
+//         if(divisor == Integer.MIN_VALUE && dividend==Integer.MAX_VALUE){
+//             return 0;
+//         }
+//         // System.out.println(b);
+//         while(a>=0){
+//             a-=b;
+//             if(a>=0){
+//                 count++;
+//             }
+//         }
+//         if(dividend<0 && divisor<0){
+//             count = count;
+//         }else if(dividend<0 || divisor<0){
+//             count = -count;
+//         }
+//         return count;
+//     }
+// }

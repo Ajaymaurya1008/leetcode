86b100b618059da24f4cd4d2e0900c1960b1250b
Ajay Maurya <ajaykumar.maurya215061@tsecol.onmicrosoft.com>
2023-11-09 00:12:34 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (69.08%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,13 +1,5 @@
-import java.util.*;
 class Solution {
     public void sortColors(int[] nums) {
         Arrays.sort(nums);
-        // int[] arr = new int[nums.length]
-        // int max=nums[0];
-        // for(int i =0; i<nums.length;i++){
-        //     for(int j = 1; j<length-i; j++) {
-
-        //     }
-        // }
     }
 }

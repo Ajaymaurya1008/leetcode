19e22510ea7d20240d293fade4b8723324a73101
Ajay Maurya <ajaykumar.maurya215061@tsecol.onmicrosoft.com>
2023-11-17 06:23:08 +0530

    Time: 17 ms (46.97%) | Memory: 44.7 MB (38.47%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -4,8 +4,6 @@ class Solution {
         for(int i =0;i<s.length();i++){
             if(map.containsKey(s.charAt(i))){
                 map.put(s.charAt(i),map.get(s.charAt(i))+1);
-                // System.out.println(map);
-                // System.out.println(map.get(s.charAt(i)));
             }else{
                 map.put(s.charAt(i),1);
             }

edec0e4405d8cb9699b1c83f171b1d06d248482f
Ajay Maurya <ajaykumar.maurya215061@tsecol.onmicrosoft.com>
2023-12-03 16:29:15 +0530

    Time: 1 ms (95.62%) | Memory: 43.2 MB (20.29%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.java
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.java
@@ -6,28 +6,7 @@ class Solution {
             int y = Math.abs(points[i][1] - points[i-1][1]);
             int ans = Math.max(x,y);
             dist += ans;
-            // int largeVal = large(points[i]);
-            // int smallVal = small(points[i-1]);
-            System.out.println(ans);
         }
         return dist;
-    }
-    static int large(int[] arr){
-        int max = Integer.MIN_VALUE;
-        for(int i:arr){
-            if(i>max){
-                max=i;
-            }
-        }
-        return max;
-    }
-    static int small(int[] arr){
-        int max = Integer.MAX_VALUE;
-        for(int i:arr){
-            if(i<max){
-                max=i;
-            }
-        }
-        return max;
     }
 }

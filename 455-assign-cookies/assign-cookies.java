2784f1eb0771965bb83bd469ebac93d34cf5e8c9
Ajay Maurya <ajaykumar.maurya215061@tsecol.onmicrosoft.com>
2024-01-01 22:33:11 +0530

    Time: 9 ms (44.73%) | Memory: 45.1 MB (17.69%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -2,18 +2,9 @@ class Solution {
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
-        int count = 0;
-        for(int i=0;i<g.length;i++){
-            for(int j=0;j<s.length;j++){
-                if(s[j]>=g[i]){
-                    count++;
-                    System.out.println(s[j]);
-                    System.out.println(g[i]);
-                    s[j]=0;
-                    break;
-                }
-            }
-        }
-        return count;
+        int i = 0;
+        for(int j=0;i<g.length && j<s.length;j++)
+	        if(g[i]<=s[j]) i++;
+        return i;
     }
 }

f09b60ec7df41822ed9496bf769656ac1abe5e43
Ajay Maurya <ajaykumar.maurya215061@tsecol.onmicrosoft.com>
2024-01-07 20:41:40 +0530

    Time: 7 ms (39.53%) | Memory: 68.8 MB (8.74%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,21 +10,41 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ArrayList<Integer> list = new ArrayList<>();
-        ListNode current = head;
-        while(current!=null){
-            list.add(current.val);
-            current=current.next;
+        if(head==null || head.next==null){
+            return true;
         }
-        int start=0;
-        int end=list.size()-1;
-        for(int i=0;i<list.size();i++){
-            if(list.get(start)!=list.get(end)){
+
+        ListNode middle = findMiddle(head);
+        ListNode reverse = reverseList(middle.next);
+        ListNode current=head;
+        while(reverse!=null ){
+            if(current.val!=reverse.val){
                 return false;
             }
-            start++;
-            end--;
+            current=current.next;
+            reverse=reverse.next;
         }
         return true;
+    }
+    public ListNode reverseList(ListNode head){
+        ListNode prev = null;
+        ListNode current = head;
+        while(current!=null){
+            ListNode nextNode=current.next;
+            current.next=prev;
+            prev=current;
+            current=nextNode;
+        }
+        return prev;
+    }
+
+    public ListNode findMiddle(ListNode head){
+        ListNode hare=head;
+        ListNode turtle=head;
+        while(hare.next!=null && hare.next.next!=null){
+            turtle=turtle.next;
+            hare=hare.next.next;
+        }
+        return turtle;
     }
 }

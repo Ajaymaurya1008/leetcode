e135e64b0c5fb0deb5c12b539230813927329ee2
Ajay Maurya <ajaykvmaurya@gmail.com>
2024-01-21 11:48:18 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (31.21%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,53 +1,13 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length==1)
-            return nums[0];
-        if(nums.length==2)
-            return Math.max(nums[0],nums[1]);
-        int a = nums[0];
-        int b = nums[1];
-        int temp = 0;
-
-        for(int i =2; i<nums.length; i++){
-            b = Math.max(a,b);
-            if(a + nums[i] >= b)
-                a += nums[i];
-            temp = a;
-            a = b;
-            b = temp;
+        int rob = 0;
+        int miss = 0;
+        for (int i = 0; i < nums.length; i ++) {
+            int rob2 = miss + nums[i];
+            int miss2 = Math.max(miss, rob);
+            rob = rob2;
+            miss = miss2;
         }
-        return Math.max(a,b);
-
-        // int even=0;
-        // int odd=0;
-        // int max=0;
-        // int m=0;
-        // for(int i =0;i<nums.length;i++){
-        //     if(i%2==0){
-        //         even += nums[i];
-        //     }
-        //     else{
-        //         odd += nums[i];
-        //     }
-        //     // even += nums[2*i];
-        //     // odd += nums[2*i+1];
-        // }
-        // if(nums.length>2){
-        //     max=nums[0]+nums[nums.length-1];
-        // }
-        // if(even>odd){
-        //     m=even;
-        // }
-        // if(odd>even){
-        //     m = odd;
-        // }
-        // if(odd==even){
-        //     m = even;
-        // }
-        // if(max>m){
-        //     m=max;
-        // }
-        // return m;
-        
+        return Math.max(rob,miss);
     }
 }

5d97e4ae3bf1b078c74db1b66eb60736cbb4efd0
Ajay Maurya <ajaykvmaurya@gmail.com>
2024-01-22 16:54:35 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (55.50%) - LeetSync

--- 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.java
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.java
@@ -5,11 +5,8 @@ class Solution {
         for(int i=0;i<target.length;i++){
             if(target[i]==count){
                 list.add("Push");
-                count++;
             }else{
                 int diff = target[i]-count;
-                // System.out.println(diff);
-                System.out.println(count);
                 for(int j=0;j<diff;j++){
                     list.add("Push");
                 }
@@ -18,9 +15,8 @@ class Solution {
                     count++;
                 }
                 list.add("Push");
-                count++;
             }
-            // count++;
+            count++;
         }
         return list;
     }

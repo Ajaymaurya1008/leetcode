b4ea82b24fe6fe284af4782e41e7dd14fdd7bd1b
Ajay Maurya <ajaykvmaurya@gmail.com>
2024-01-22 16:57:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (21.75%) - LeetSync

--- 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.java
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.java
@@ -3,19 +3,15 @@ class Solution {
         ArrayList<String> list = new ArrayList<>();
         int count=1;
         for(int i=0;i<target.length;i++){
-            if(target[i]==count){
-                list.add("Push");
-            }else{
+            if(target[i]!=count){
                 int diff = target[i]-count;
                 for(int j=0;j<diff;j++){
                     list.add("Push");
-                }
-                for(int j=0;j<diff;j++){
                     list.add("Pop");
                     count++;
                 }
-                list.add("Push");
             }
+            list.add("Push");
             count++;
         }
         return list;

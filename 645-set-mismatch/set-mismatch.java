4ab552e82bd051146b905434a56783bf5671b713
Ajay Maurya <ajaykvmaurya@gmail.com>
2024-01-22 11:26:57 +0530

    Time: 10 ms (25.07%) | Memory: 45.7 MB (23.87%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,40 +1,18 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
         int[] arr = new int[2];
-        // Arrays.sort(nums);
-        // // System.out.println(Arrays.toString(nums));
-        // for(int i=1;i<nums.length;i++){
-        //     if(nums[i]==nums[i-1]){
-        //         arr[0]=nums[i];
-        //     }
-        // }
-        // HashSet<Integer> set = new HashSet<>();
-        // for(int i:nums) set.add(i);
-        // Integer[] num = set.toArray(new Integer[set.size()]); 
-        // System.out.println(Arrays.toString(num));
-        // for(int i=0;i<num.length;i++){
-        //     if(num[i]!=i+1){
-        //         arr[1]=i+1;
-        //         break;
-        //     }
-        // }
-        // if(arr[1]==0) arr[1]=nums.length;
-        HashSet<Integer> list = new HashSet<>();
-        int n=1;
-        while(n<=nums.length){
-            list.add(n);
-            n++;
-        } 
+        int sum1=0,sum2=0;
+        for(int i=1;i<=nums.length;i++) sum1+=i;
         HashSet<Integer> set = new HashSet<>();
         for(int i:nums) {
-            list.remove(i);
             if(set.contains(i)){
                 arr[0]=i;
             }else{
                 set.add(i);
+                sum2+=i;
             }
         }
-        for(int i:list) arr[1]=i;
+        arr[1]=sum1-sum2;
         return arr;
     }
 }

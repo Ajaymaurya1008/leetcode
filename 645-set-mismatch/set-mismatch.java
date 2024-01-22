fd026600af1322bc23e3db6bff9ff04b9d94eed0
Ajay Maurya <ajaykvmaurya@gmail.com>
2024-01-22 11:20:57 +0530

    Time: 22 ms (5.09%) | Memory: 46.5 MB (8.03%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,34 +1,40 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
         int[] arr = new int[2];
-        Arrays.sort(nums);
-        // System.out.println(Arrays.toString(nums));
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]==nums[i-1]){
-                arr[0]=nums[i];
-            }
-        }
-        HashSet<Integer> set = new HashSet<>();
-        for(int i:nums) set.add(i);
-        Integer[] num = set.toArray(new Integer[set.size()]); 
-        System.out.println(Arrays.toString(num));
-        for(int i=0;i<num.length;i++){
-            if(num[i]!=i+1){
-                arr[1]=i+1;
-                break;
-            }
-        }
-        if(arr[1]==0) arr[1]=nums.length;
-
-        // HashSet<Integer> set = new HashSet<>();
-        // for(int i:nums) {
-        //     if(set.contains(i)){
-        //         arr[0]=i;
-        //     }else{
-        //         set.add(i);
+        // Arrays.sort(nums);
+        // // System.out.println(Arrays.toString(nums));
+        // for(int i=1;i<nums.length;i++){
+        //     if(nums[i]==nums[i-1]){
+        //         arr[0]=nums[i];
         //     }
         // }
-        // System.out.println(set);
+        // HashSet<Integer> set = new HashSet<>();
+        // for(int i:nums) set.add(i);
+        // Integer[] num = set.toArray(new Integer[set.size()]); 
+        // System.out.println(Arrays.toString(num));
+        // for(int i=0;i<num.length;i++){
+        //     if(num[i]!=i+1){
+        //         arr[1]=i+1;
+        //         break;
+        //     }
+        // }
+        // if(arr[1]==0) arr[1]=nums.length;
+        HashSet<Integer> list = new HashSet<>();
+        int n=1;
+        while(n<=nums.length){
+            list.add(n);
+            n++;
+        } 
+        HashSet<Integer> set = new HashSet<>();
+        for(int i:nums) {
+            list.remove(i);
+            if(set.contains(i)){
+                arr[0]=i;
+            }else{
+                set.add(i);
+            }
+        }
+        for(int i:list) arr[1]=i;
         return arr;
     }
 }

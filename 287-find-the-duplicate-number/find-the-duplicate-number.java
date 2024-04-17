51960800f9daf4b15c2fa054e2c9a55671d2ca74
Ajay Maurya <ajaykvmaurya@gmail.com>
2024-04-17 21:03:44 +0530

    Time: 42 ms (6.61%) | Memory: 60.7 MB (5.12%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -4,7 +4,6 @@ class Solution {
         for(int i:nums){
             map.put(i,map.getOrDefault(i,0)+1);
         }
-        System.out.println(map);
         for(int i:nums){
             if (map.get(i)>1) return i;
         }

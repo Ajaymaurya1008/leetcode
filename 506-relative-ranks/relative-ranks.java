9aa721e650ed3dab80bc6130a3338c93eb2f6a42
Ajay Maurya <ajaykvmaurya@gmail.com>
2024-05-08 12:25:28 +0530

    Time: 43 ms (15.28%) | Memory: 45.4 MB (84.78%) - LeetSync

--- 506-relative-ranks/relative-ranks.java
+++ 506-relative-ranks/relative-ranks.java
@@ -7,8 +7,6 @@ class Solution {
         for(int i=0;i<sortedArr.length;i++){
             map.put(sortedArr[i],sortedArr.length-i);
         }
-        System.out.println(map);
-        System.out.println(Arrays.toString(sortedArr));
         for(int i=0;i<score.length;i++){
             System.out.println(map.get(score[i]));
             if(map.get(score[i])==1){
